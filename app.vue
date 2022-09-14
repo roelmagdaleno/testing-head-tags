5bfcb1a0eb204e053232305b17a34e714d03799b
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-09-14 10:31:04 -0500

    Initial files

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+	  <h1>Head Tags</h1>
+	  <input type="text" v-model="website">
+	  <button type="button" @click="getHeadTags">Get head tags</button>
+  </div>
+</template>
+
+<script setup>
+const website = ref('https://wp-bullet.com');
+
+function getHeadTags() {
+	const response = $fetch(website.value);
+	console.log(response)
+}
+</script>
